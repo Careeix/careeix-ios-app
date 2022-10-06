4c6e093e5147ea5094688189623d50574891ad50
김지훈 <ben224@naver.com>
2022-10-06 21:47:52 +0900

    [추가] 제네릭 Moya 추가

---
+++ Careeix/Careeix/Networking/NetworkService.swift
@@ -0,0 +1,91 @@
+//
+//  NetworkService.swift
+//  Careeix
+//
+//  Created by 김지훈 on 2022/10/06.
+//
+
+import Foundation
+import Moya
+
+enum CustomTask {
+    case requestPlain
+    case requestJSONEncodable(Encodable)
+    case requestParameters(encoding: ParameterEncoding)
+}
+struct APIResponse<T: Decodable>: Decodable {
+    let code: Int?
+    let data: T?
+    let message: String?
+}
+
+struct ServiceAPI: TargetType {
+    var path: String
+    var method: Moya.Method
+    var parameters: [String: Any]
+    var baseURL: URL { return URL(string: "https://reqres.in")! }
+    var task: Moya.Task
+    var headers: [String : String]?
+    
+}
+class API<T: Decodable> {
+    
+    let api: ServiceAPI
+    private let provider = MoyaProvider<MultiTarget>()
+    init(
+        path: String,
+        method: Moya.Method,
+        parameters: [String: Any],
+        task: CustomTask,
+        headers: [String: String]? = nil
+    ) {
+        var newTask: Moya.Task = .requestPlain
+        switch task {
+        case .requestPlain:
+            newTask = .requestPlain
+        case .requestJSONEncodable(let requestModel):
+            newTask = .requestJSONEncodable(requestModel)
+        case .requestParameters(let encodingType):
+            newTask = .requestParameters(parameters: parameters, encoding: encodingType)
+        }
+        self.api = .init(path: path, method: method, parameters: parameters, task: newTask, headers: headers)
+    }
+    
+    func request(completion: @escaping (Result<APIResponse<T>, Error>) -> Void) {
+        let endpoint = MultiTarget.target(api)
+        provider.request(endpoint, completion: { result in
+            switch result {
+            case .success(let response):
+                do {
+                    try self.httpProcess(response: response)
+                    
+                    let data = try response.map(APIResponse<T>.self)
+                    
+                    completion(.success(data))
+                } catch NetworkError.httpStatus(let code){
+                    print("Network Error Code: ", code)
+                } catch (let error) {
+                    print("디폴트 에러: ", error.localizedDescription)
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                print("??? 에러: ", error.localizedDescription)
+                completion(.failure(error))
+            }
+        })
+    }
+    
+    private func httpProcess(response: Response) throws {
+        guard 200...299 ~= response.statusCode else {
+            throw NetworkError.httpStatus(response.statusCode)
+        }
+    }
+    
+    
+}
+public enum NetworkError: Error {
+    case objectMapping // 일반 데이터 파싱에서 오류가 나는 경우
+    case httpStatus(Int) // statusCode가 200...299 밖에 나오는 경우
+}
+
+
