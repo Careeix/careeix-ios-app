06f605393116e3d5d7beffaf886dc08c2e5003d0
hansm <hanmix2003@naver.com>
2022-10-20 06:02:01 +0900

    [삭제] HomeViewModel 삭제

--- Careeix/Careeix/Home/HomeViewModel.swift
+++ Careeix/Careeix/Home/HomeViewModel.swift
@@ -8,14 +8,5 @@
 import Foundation
 
 class HomeViewModel {
-    let minimalCareerProfile: [CareerModel] = [CareerModel(profileImage: "person", nickname: "키키", careerName: "UI 디자이너", careerGrade: "주니어(1~4년차)", detailCareerName: ["#ui디자이너", "#ux디자이너", "#uiux디자이너"])]
     
-    let releventCareerProfiles: [RelevantCareerModel] = [
-        RelevantCareerModel(careerName: "서버개발자", careerGrade: "주니어(1~4년차)", detailCareerName: ["자바", "스프링", "Java Spring"]),
-        RelevantCareerModel(careerName: "iOS 개발자", careerGrade: "주니어(1~4년차)", detailCareerName: ["스위프트", "swift", "iOS"]),
-        RelevantCareerModel(careerName: "UI 디자이너", careerGrade: "주니어(1~4년차)", detailCareerName: ["ui디자이너", "ux디자이너", "uiux디자이너"]),
-        RelevantCareerModel(careerName: "안드로이드 \n개발자", careerGrade: "주니어(1~4년차)", detailCareerName: ["코틀린", "자바", "안드로이드"]),
-        RelevantCareerModel(careerName: "기획자", careerGrade: "주니어(1~4년차)", detailCareerName: ["레벨", "기획"]),
-        RelevantCareerModel(careerName: "프로젝트 \n매니저", careerGrade: "주니어(1~4년차)", detailCareerName: ["PM"])
-    ]
 }
