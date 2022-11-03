24b2075bcc2f5e20d77d6e3884971e2b3240d689
hansm <hanmix2003@naver.com>
2022-11-04 03:30:10 +0900

    [수정] 마이페이지 테이블뷰 셀 수정

--- Careeix/Careeix/MyPage/Cells/MyPageTableViewCell.swift
+++ Careeix/Careeix/MyPage/Cells/MyPageTableViewCell.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class MyPageTableViewCell: UITableViewCell {
- 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
@@ -16,10 +15,4 @@ class MyPageTableViewCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-//        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 30, left: 25, bottom: 30, right: 0))
-    }
-
 }
